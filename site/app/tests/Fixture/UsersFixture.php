4fe8545450e467d04be04a585df2c4d337f9fa5f
arthursiq5 <arthur.siq5@gmail.com>
2022-06-19 20:13:50 -0300

    Criado scaffold para tabela Users

---
+++ site/app/tests/Fixture/UsersFixture.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ */
+class UsersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'nome' => 'Lorem ipsum dolor sit amet',
+                'email' => 'Lorem ipsum dolor sit amet',
+                'senha' => 'Lorem ipsum dolor sit amet',
+                'ativo' => 1,
+                'created' => '2022-06-19 23:13:24',
+                'updated' => '2022-06-19 23:13:24',
+            ],
+        ];
+        parent::init();
+    }
+}
